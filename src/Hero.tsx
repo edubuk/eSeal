8aada865fccaf2cfa752bbbc2a58dabbf31f4dec
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2023-09-06 12:54:19 +0530

    Added Confetti on eSeal your Certificate button

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -9,6 +9,59 @@ export function Hero() {
     borderRadius: "8px",
   };
 
+  async function Confetti() {
+    const confetti_style = {
+      spread: 360,
+      ticks: 70,
+      gravity: 0,
+      decay: 0.90,
+      startVelocity: 25,
+      colors: ["#b0f7f2", "#f4afd4", "#ffc8ae", "#b7f7bf", "#dfb4f2"],
+      origin: {
+        x: 0.5,
+        y: 0.35,
+      },
+    }
+
+    function loadConfetti() {
+      return new Promise<(opts: any) => void>((resolve, reject) => {
+        if ((globalThis as any).confetti) {
+          return resolve((globalThis as any).confetti as any);
+        }
+        const script = document.createElement("script");
+        script.src =
+          "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
+        script.onload = () =>
+          resolve((globalThis as any).confetti as any);
+        script.onerror = reject;
+        document.head.appendChild(script);
+        script.remove();
+      });
+    }
+
+    const confetti = await loadConfetti();
+
+    function shoot() {
+      confetti({
+        ...confetti_style,
+        particleCount: 80,
+        scalar: 1.2,
+      });
+
+      confetti({
+        ...confetti_style,
+        particleCount: 60,
+        scalar: 0.75,
+      });
+    }
+
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+      setTimeout(shoot, 300);
+      setTimeout(shoot, 400);
+  }
+
   return (
     <>
       <div className="container hero" style={{paddingTop: "5vh"}}>
@@ -16,7 +69,7 @@ export function Hero() {
           <h1>Edubuk<br />eSeal dApp</h1>
           <p style={{fontSize: "24px", paddingTop: "3vh", textAlign: "center"}}><span className="highlight">Powered by Concordium</span></p>
           <div className="button-group" style={{justifyContent: "center", paddingTop: "3vh"}}>
-            <a href="#sealer"><button className="button" style={{backgroundColor: "#7303fc", padding: "1em"}}>eSeal Your Certificates</button></a>
+            <a href="#sealer"><button className="button" style={{backgroundColor: "#7303fc", padding: "1em"}} onClick={(e: any) => Confetti()}>eSeal Your Certificates</button></a>
           </div>
         </span>
       </div>
